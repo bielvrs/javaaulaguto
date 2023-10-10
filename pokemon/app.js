0679ee8a0224830700dc856f92fe99627381425e
bielvrs <gabrielvrs031@gmail.com>
2023-10-09 21:47:32 -0300

    pokemons alternativa certa

---
+++ pokemon/app.js
@@ -0,0 +1,14 @@
+function VerificarResposta(){
+    let respostaCerta
+    respostaCerta = document.getElementById ("alternativacerta").checked
+
+    console.log(respostaCerta)
+
+    if(respostaCerta == true){
+        alert("Parabéns!!!\nVocê Acertou.")
+}else{
+    alert("Que pena, não foi dessa vez.")
+    }
+    
+}
+

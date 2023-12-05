4f6c8cc70188bb58cd06dcafd5168357f787ae09
bielvrs <gabrielvrs031@gmail.com>
2023-12-04 22:26:06 -0300

    racionais

---
+++ aula 04/12/app.js
@@ -0,0 +1,14 @@
+function jedi() {
+    document.getElementById("mensagem").innerText = "Venha para o lado da luz"
+    document.getElementById("frase").innerText = "Guerras Não Fazem Ninguém Maior"
+    document.getElementById("imagem").src="jedi.gif"
+    document.body.style="background: #607479"
+}
+
+function sith(){
+    document.getElementById("mensagem").innerText = "Venha para o lado sombrio"
+    document.getElementById("frase").innerText = "A paz é uma mentira só a emoção importa"
+    document.getElementById("imagem").src="sith.gif"
+    document.body.style="background: #4f5055"
+
+}
---
+++ aula 04/12/exercício.js
@@ -0,0 +1,16 @@
+function coresevalores() {
+    document.getElementById("mensagem").innerText = "Racionais Mcs - Cores e Valores"
+    document.getElementById("frase").innerText = "Cores & Valores é o quarto álbum de estúdio do grupo brasileiro de rap Racionais MCs, lançado em 25 de novembro de 2014 pelas gravadoras Cosa Nostra e Boogie Naipe1. Diferentemente dos álbuns anteriores, que eram marcados por várias canções de longa duração, Cores & Valores apresenta diversos raps curtos e diretos. A faixa mais extensa é “O Mal e o Bem”, com exatos cinco minutos. Este álbum é o primeiro com faixas inéditas desde Nada como um Dia após o Outro Dia, lançado 12 anos antes, e conta com 15 músicas12. A capa do disco apresenta Mano Brown, Ice Blue, Edi Rock e KL Jay empunhando armas, vestidos com roupas de coletores de lixo e mascarados. A foto foi tirada ao lado de um banco que fica na base do Edifício Copan, no centro de São Paulo."
+    document.getElementById("imagem").src="cores.jpg"
+    document.body.style="background: #607479"
+}
+
+function sobrevivendonoinferno(){
+    document.getElementById("mensagem").innerText = "Racionais Mcs - Sobrevivendo no inferno"
+    document.getElementById("frase").innerText = "“Sobrevivendo no Inferno” é um álbum icônico lançado pelo grupo de rap Racionais MC’s em 1997. Com mais de 200 mil cópias vendidas no mês de estreia, o disco logo se tornou um sucesso. Segundo o próprio grupo, o álbum vendeu cerca de 1,5 milhão de cópias oficialmente e explodiu no mercado alternativo, com mais de quatro milhões de vendas. Impressionantemente, o CD foi lançado por uma gravadora independente chamada Cosa Nostra, que já havia trabalhado com outros grupos de rap. Desde então, “Sobrevivendo no Inferno” permanece como uma obra relevante e importante para a cultura brasileira."
+
+    "O álbum é composto por 12 músicas que abordam temas sociais, políticos e raciais. As letras falam sobre a vida nas periferias, o racismo, a violência policial, o encarceramento em massa, o mundo das drogas e a desigualdade social. Cada faixa oferece uma visão crua e direta do cotidiano de quem vive nas “quebradas”. A relevância e o impacto desse álbum continuam a ressoar até os dias atuais, tornando-o uma verdadeira disrupção na música brasileira"
+    document.getElementById("imagem").src="sobrevivendo.jpg"
+    document.body.style="background: #4f5055"
+
+}
